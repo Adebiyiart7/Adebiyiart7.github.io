feb0ee7a3a897fe6ff448b9283b89739faa5e2a6
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-01 22:50:30 +0100

    Added animation

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -5,19 +5,19 @@ import AppButton from "../reusable/AppButton";
 
 const useStyle = makeStyles({
   actionContainer: {
-    display: "block",
+    display: "block"
   },
   greeting: {
     fontSize: 20,
     fontWeight: 500,
     color: "var(--white)",
-    textShadow: "0 0 5px #000",
+    textShadow: "0 0 5px #000"
   },
   landingContainer: {
     display: "flex",
     justifyContent: "space-between",
     marginTop: 30,
-    alignItems: "center",
+    alignItems: "center"
   },
   name: {
     fontWeight: 800,
@@ -25,18 +25,18 @@ const useStyle = makeStyles({
     textTransform: "uppercase",
     fontFamily: "var(--appFontFamily)",
     color: "var(--white)",
-    textShadow: "0 0 5px #000",
+    textShadow: "0 0 5px #000"
   },
   photo: {
     maxWidth: 370,
     width: "100%",
-    borderRadius: "50%",
+    borderRadius: "50%"
   },
   right: {},
   title: {
     color: "rgb(152, 152, 152)",
-    fontWeight: 500,
-  },
+    fontWeight: 500
+  }
 });
 
 const Landing = ({ mediaQueries }) => {
@@ -46,35 +46,41 @@ const Landing = ({ mediaQueries }) => {
   // media queries
   const styles = {
     actionContainer: {
-      marginTop: tablet ? 50 : 100,
+      marginTop: tablet ? 50 : 100
     },
     landingContainer: {
       flexDirection: tablet ? "column" : "row",
       textAlign: tablet ? "center" : "initial",
-      padding: largeUp ? "0 20px" : "initial",
+      padding: largeUp ? "0 20px" : "initial"
     },
     name: {
       fontWeight: medium && 700,
       fontSize: medium && 35,
-      lineHeight: large && 1.2,
-    },
+      lineHeight: large && 1.2
+    }
   };
 
   return (
     <div className={classes.landingContainer} style={styles.landingContainer}>
-      <div className={classes.left}>
+      <div className={`${classes.left}`}>
         <div>
-          <div>
-            <h5 className={classes.greeting}>Hello, Welcome to Xtraaa Technology</h5>
+          <div
+            className="wow fadeInLeft"
+            data-wow-delay="0ms"
+            data-wow-duration="1000ms"
+          >
+            <h5 className={classes.greeting}>
+              Hello, meet <strong>theCodingChef</strong>.
+            </h5>
             <h1 className={classes.name} style={styles.name}>
               I am Adeeyo Joseph
             </h1>
-            <p className={classes.title}>
-              Full Stack Developer
-            </p>
+            <p className={classes.title}>Software Developer</p>
           </div>
           <div
-            className={classes.actionContainer}
+            className={`${classes.actionContainer} wow fadeInUp`}
+            data-wow-delay="300ms"
+            data-wow-duration="1000ms"
             style={styles.actionContainer}
           >
             <a href="/">
@@ -83,7 +89,11 @@ const Landing = ({ mediaQueries }) => {
           </div>
         </div>
       </div>
-      <div className={classes.right}>
+      <div
+        className={`${classes.right} wow fadeInRight`}
+        data-wow-delay="300ms"
+        data-wow-duration="1000ms"
+      >
         <img className={classes.photo} src={photo} alt="" />
       </div>
     </div>
