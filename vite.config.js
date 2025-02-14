d781be12ba771eb69c38bf4d15b9ecf2f54dc915
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2025-02-14 06:58:20 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./", // ensure relative paths, e.g. in index.html
   build: {
-    outDir: "dist", // gets deployed to github pages
+    outDir: "docs", // gets deployed to github pages
     assetsDir: "assets", // static assets (images, js, css, etc.)
   },
   assetsInclude: ["**/*.apk"],
